bd61570059471321e688c668ab88985b275a4ab1
Sarah <sarahhale95@gmail.com>
2018-08-30 14:09:30 +0200

    read from stdin

--- main.c
+++ main.c
@@ -3,25 +3,27 @@
  *  Laboration <n> <kurs> <termin>
  *  <Kort enradsbeskrivning av filens innehåll och syfte>
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "linkedlist.h"
+#define BUFSIZE 1023
+void read(FILE * fp);
 int main(int argc, char *argv[]){
-
   if(argv[1] != NULL){
     FILE * fp;
     fp = fopen(argv[1],"r");
-
-    char singleLine[1023];
-    while(fgets(singleLine, 1023, fp) != NULL){
-      puts(singleLine);
-    }
-
+    read(fp);
     fclose(fp);
+
   }else{
-    showList(createLinkedList(3));
-    printf("%s\n","NO INPUT FILE" );
+    read(stdin);
   }
   return 0;
+}
+
+void read(FILE * fp){
+  char buffer[BUFSIZE];
+  while(fgets(buffer, BUFSIZE, fp) != NULL){
+    puts(buffer);
+  }
 }
